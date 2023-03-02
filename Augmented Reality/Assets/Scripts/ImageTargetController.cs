fdd53c9da23e2bbc4ee217409bf87e6c633549d6
JonIbarra <71033898+JonIbarra@users.noreply.github.com>
2023-03-02 13:00:45 +0100

    Enlarged basket and added project settings

--- Augmented Reality/Assets/Scripts/ImageTargetController.cs
+++ Augmented Reality/Assets/Scripts/ImageTargetController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnSpheres : MonoBehaviour
+public class ImageTargetController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
