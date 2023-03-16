e1edd56db19f718ae111e61e4fb9625861f380cb
JonIbarra <71033898+JonIbarra@users.noreply.github.com>
2023-03-16 16:13:01 +0100

    Balling
    Balls can enter through the hoop let's GOOOOOOOOOOOOOOOO

--- Augmented Reality/Assets/SpawnSpheres.cs
+++ Augmented Reality/Assets/SpawnSpheres.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class SpawnSpheres : MonoBehaviour
 {
     public GameObject prefab;
-    public Transform imageTransform;
+    Camera mCamera;
+
     // Start is called before the first frame update
     void Start()
     {
-        prefab = Instantiate(prefab, new Vector3(imageTransform.position.x, imageTransform.position.y, imageTransform.position.z + 7.5f), Quaternion.identity);
+        mCamera = GetComponent<Camera>();
+        Vector3 offset = mCamera.transform.forward * 5;
+        
+        //prefab = Instantiate(prefab, new Vector3(transform.position.x + offset.x, transform.position.y + offset.y, transform.position.z + offset.z), transform.rotation);
     }
 
     // Update is called once per frame
--- Augmented Reality/Assets/Swipe.cs
+++ Augmented Reality/Assets/Swipe.cs
@@ -6,30 +6,61 @@ public class Swipe : MonoBehaviour
 {
     Vector2 startPos;
     Vector2 endPos;
-    Vector2 dir;
 
     float touchStartTime;
     float touchEndTime;
     float timeInterval;
 
-    float throwForceInXandY = 1f;
-    float throwForceInZ = 50f;
+    public float throwForceForward;
+    public float throwForceUp;
 
     Rigidbody rb;
+
+    public Transform mCamTransform;
+    public GameObject mTarget;
+    bool physics_active;
+    bool active;
+
+    Vector3 dir;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        ResetBall();
+        active = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && active)
         {
-            touchStartTime = Time.time;
-            startPos = Input.GetTouch(0).position;
+            if (!physics_active)
+            {
+                dir = mCamTransform.forward * throwForceForward + mCamTransform.up * throwForceUp;
+
+                rb.useGravity = true;
+                rb.isKinematic = false;
+                rb.velocity = dir;
+                Physics.gravity = mTarget.transform.up * (-1000);
+
+                //transform.position = dir;
+
+                physics_active = true;
+                Invoke("ResetBall", 5);
+            }
+            else
+            {
+                ResetBall();
+            }
+        }
+        if (!physics_active)
+        {
+            Vector3 offset = mCamTransform.forward * 250;
+            Debug.Log("Location: " + offset);
+
+            transform.position = new Vector3(mCamTransform.position.x + offset.x, mCamTransform.position.y + offset.y, mCamTransform.position.z + offset.z);
         }
 
         if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
@@ -39,14 +70,26 @@ public class Swipe : MonoBehaviour
 
             endPos = Input.GetTouch(0).position;
 
-            dir = startPos - endPos;
-
-            rb.useGravity = true;
-            rb.isKinematic = false;
-            rb.AddForce(-dir.x * throwForceInXandY, -dir.y * throwForceInXandY, throwForceInZ / timeInterval);
-
-            Destroy(gameObject, 3f);
-
         }
+    }
+
+    void ResetBall()
+    {
+        if (physics_active)
+        {
+            physics_active = false;
+            rb.useGravity = false;
+            rb.isKinematic = false;
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+    }
+
+    public void ThrowActivate()
+    {
+        active = true;
+    }
+    public void ThrowDeactivate()
+    {
+        active = false;
     }
 }
