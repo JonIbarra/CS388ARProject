350fbf729c9f28cf09cdb6bd4229d894ba962654
AsierAzpiri <asier.azpiri@digipen.edu>
2023-03-16 11:30:00 +0100

    Ball swipes when dragging

--- Augmented Reality/Assets/SpawnSpheres.cs
+++ Augmented Reality/Assets/SpawnSpheres.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class SpawnSpheres : MonoBehaviour
 {
+    public GameObject prefab;
+    public Transform imageTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        prefab = Instantiate(prefab, new Vector3(imageTransform.position.x, imageTransform.position.y, imageTransform.position.z + 7.5f), Quaternion.identity);
     }
 
     // Update is called once per frame
